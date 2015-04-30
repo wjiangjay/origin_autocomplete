dfc93bec91e2fa744c93b5cfc43a8555e89daed9
Weiwei Jiang <wjiang@redhat.com>
2015-04-30 14:50:17 +0800

    work well version

--- generator.sh
+++ generator.sh
@@ -1,6 +1,6 @@
-#!/bin/env bash 
-## use to generate the bash autocomplete script
+#!/bin/env bash
 
+## use to generate the bash autocomplete script
 
 ## TODO : some command use `command options`
 
@@ -19,19 +19,40 @@ _process_function()
         valid_commands=(`$command --help 2>&1|grep -A 100 "Available Commands:"|grep -B 100 "Options:"|grep -B 100 "^$"|grep -v "Available Commands:"|grep -v ^$ |awk '{print $1}'`)
     fi
     valid_options=(`$command --help 2>&1|grep -A 100 "Options"|grep -B 100 "^$"|grep -v "Options:"|grep -v ^$ |awk -F= '{print $1}'`)
+
+
+    if [[ $command == $cmd ]];then
+            $(cat >> $output <<EOF
+    if [ \$COMP_CWORD -eq 1 ]; then
+        if [[ "\$cur" == -* ]]; then
+            opts="$(_display ${valid_options[*]})"
+        elif [ -z \$cur ]; then
+            opts="$(_display ${valid_commands[*]})"
+        else
+            opts="$(_display ${valid_commands[*]})"
+        fi
+    else
+        prev="\${COMP_WORDS[@]:0:COMP_CWORD}"
+        SAVE_IFS=\$IFS
+        IFS=" "
+        case "\${prev[*]}" in)
+    else
+        `cat >> $output <<EOF
+        "$command")
+            if [[ "\\$cur" == -* ]];then
+                opts="$(_display ${valid_options[*]})"
+            else
+                opts="$(_display ${valid_commands[*]})"
+            fi
+        ;;`
+    fi
+
     if [[ $valid_commands != "" ]];
     then
-        if [[ $valid_options != "" ]];
-        then
-            _display ${valid_options[*]}
-        fi
-        _display ${valid_commands[*]}
         for valid_command in ${valid_commands[*]};
         do
-            _process_function "$command $valid_command" 
-        done 
-    else
-        _display ${valid_options[*]}
+            _process_function "$command $valid_command"
+        done
     fi
 }
 
@@ -40,14 +61,34 @@ _display()
     echo $*|tr , " "
 }
 
-_valid_command()
+cmd=$1
+output=$2
+if [[ `which $cmd` == "" ]];then
+    echo "The command you provide \"$cmd\" is not existed, please check"
+else
+    if [[ $# -le 1 ]];then
+        echo "Do not give enough arguments."
+    else
+        $(cat >> $output <<EOF
+_${cmd}()
 {
-   echo "" 
+    local cur opts prev
+    COMPREPLY=()
+    cur="\${COMP_WORDS[COMP_CWORD]}")
+        _process_function $1
+    $(cat >> $output <<EOF
+        esac
+        IFS=\$SAVE_IFS
+    fi
+
+    COMPREPLY=( \$(compgen -W "\${opts}" -- \${cur}))
+    return 0
 }
 
-_valid_options()
-{
-    echo ""
-}
+    complete -o default -F _${cmd} ${cmd}
 
-_process_function $1
+
+
+)
+    fi
+fi
